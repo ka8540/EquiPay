fcfb9141f4861a111933901041698dc089ac6d03
ka8540 <ka8540@g.rit.edu>
2024-06-10 14:54:25 -0700

    edit FriendDashboard

--- equipay/FriendsDashBoard.js
+++ equipay/FriendsDashBoard.js
@@ -128,7 +128,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#f5f5f5',
+    backgroundColor: '#fff',
     padding: 20,
   },
   profileCard: {
